057408a7596b8c45b7783d518a7c1461851938f8
malfo902 <m.malfatto@icloud.com>
2025-07-28 19:07:33 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# Inserisci qui le tue API Key Binance Futures
+BINANCE_API_KEY = "cyF4T8OTEomDm5L8nBX9AweNjOwkLWoObDn8uzlhSSpwS0MuFtyJGEaIe283bSQZ"
+BINANCE_API_SECRET = "NhKbFEuTVe9TXVa9fuj0Q2JJP1af5dfjuII3oMJ7uRp5ZhtrTwTi60tL5VJn0Bkf"
