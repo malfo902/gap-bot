c5b9d86667c3d4b3b1f4dd3dcd15796f0a4dd185
malfo902 <m.malfatto@icloud.com>
2025-07-29 20:37:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 
-BINANCE_API_KEY = os.getenv("")
-BINANCE_API_SECRET = os.getenv("")
+BINANCE_API_KEY = os.getenv("cyF4T8OTEomDm5L8nBX9AweNjOwkLWoObDn8uzlhSSpwS0MuFtyJGEaIe283bSQZ")
+BINANCE_API_SECRET = os.getenv("NhKbFEuTVe9TXVa9fuj0Q2JJP1af5dfjuII3oMJ7uRp5ZhtrTwTi60tL5VJn0Bkf")
 
+# Asset configurazione
 SYMBOL = "ETHUSDC"
 QUOTE_ASSET = "USDC"
 BASE_ASSET = "ETH"
 
+# Percentuale di investimento per ogni livello (es: 4% del saldo USDC disponibile)
 TRADE_PERCENTAGE = 0.04
+
+# Sicurezza e webhook
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 
-POLL_INTERVAL = 10  # secondi tra controlli prezzo
+# Frequenza del controllo prezzo (in secondi)
+POLL_INTERVAL = 10
+
